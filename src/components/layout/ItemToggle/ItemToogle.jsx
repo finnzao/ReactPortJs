020dba818529680cf10b119651b2f17159f2f3a0
finnzao <lipefelipe403@hotmail.com>
2023-01-29 12:37:08 -0300

    fixed button

--- src/components/layout/ItemToggle/ItemToogle.jsx
+++ src/components/layout/ItemToggle/ItemToogle.jsx
@@ -10,9 +10,9 @@ function ItemToggle(props) {
 
     return (
         <div className={styles.box}>
-            <div className={styles.container}>
+            <div className={styles.container} onClick={ToggleMode}>
                 <div className={styles.boxTitle}>
-                    <h3>{props.title}</h3><div className={styles.button} onClick={ToggleMode} >
+                    <h3>{props.title}</h3><div className={styles.button}  >
 
                         <svg className={active ? `${styles.open}` : `${styles.close}`} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                             <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z" />
