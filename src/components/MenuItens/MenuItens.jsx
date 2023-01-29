8f3b0bccc3f98a1828ca7bf9fee88889fbb70a2c
Felipe <79268565+finnzao@users.noreply.github.com>
2023-01-28 23:30:41 -0300

    Add files via upload

--- src/components/MenuItens/MenuItens.jsx
+++ src/components/MenuItens/MenuItens.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
 import styles from "./MenuItens.module.css"
 function contactAuthor() {
+    const Displaytop = () => {
+        window.scrollTo(0, 0)
+    }
     return (
         <div className={styles.contactAuthor}>
-            <div className={styles.itemContact}><Link to="./">HOME</Link></div>
-            <div className={styles.itemContact}><Link to="./portfolio">PORTFOLIO</Link></div>
-            <div className={styles.itemContact}><Link to="./about">SOBRE</Link></div>
+            <div className={styles.itemContact} onClick={Displaytop}><Link to="./">HOME</Link></div>
+            <div className={styles.itemContact} onClick={Displaytop}><Link to="./portfolio">PORTFOLIO</Link></div>
+            <div className={styles.itemContact} onClick={Displaytop}><Link to="./about">SOBRE</Link></div>
         </div >
     )
 }
